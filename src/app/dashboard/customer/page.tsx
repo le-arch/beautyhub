5d32a3b5e5ddef88bcabb1ac369a10fe72592d3e
leonora basil <basilleonora@gmail.com>
2025-08-25 11:57:41 +0000

    place back all the features on the customer dashboard nothing seems to s

--- src/app/dashboard/customer/page.tsx
+++ src/app/dashboard/customer/page.tsx
@@ -1,3 +1,15 @@
+import AiStylist from "@/components/ai-stylist";
+import FeaturedCategories from "@/components/featured-categories";
+import HeroSection from "@/components/hero-section";
+import TopRatedSalons from "@/components/top-rated-salons";
+
 export default function CustomerDashboard() {
-  return null;
+  return (
+    <>
+        <HeroSection />
+        <FeaturedCategories />
+        <TopRatedSalons />
+        <AiStylist />
+    </>
+  );
 }
